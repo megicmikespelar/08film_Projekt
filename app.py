aea076318a775b541e4d74819e2e5d3a1e263a65
JakobNyholmNeander <jakob.nyholmneander@elev.ga.lbs.se>
2024-03-28 11:48:33 +0100

    kommentera

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from database import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')# Variabel som sorterar
 def hello():
-    sort_by = request.args.get('sort_by')
+    sort_by = request.args.get('sort_by')# hjälper att sortera
     data = get_all_movies(sort_by)
     return render_template('index.html', movies=data, sort_by=sort_by)
 
